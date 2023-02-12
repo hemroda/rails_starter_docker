96e2e1a040b36172690ba8f32548357b5740b9d0
hemroda <hemroda@gmail.com>
2023-02-12 17:00:04 +0100

    ensure migration is run when server is running

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -7,3 +7,8 @@ if [ -f tmp/pids/server.pid ]; then
 fi
 
 bundle exec rails s -b 0.0.0.0
+
+# If the rails server is running then create or migrate existing database
+if [ "${*}" == "./bin/rails server" ]; then
+  ./bin/rails db:prepare
+fi
